4522455ab39453ccbff5f491765851becef8f93b
Nari07 <mickinguyen12@gmail.com>
2023-04-06 21:43:42 -0500

    add switch case for operators; add pos/neg function; fix bug where multiply & delete don't work when operator is deleted

--- script2.js
+++ script2.js
@@ -1,28 +1,61 @@
 const buttons = document.getElementsByClassName('number');
 const displayCalculation = document.getElementById('displayCalculation');
 const displayInput = document.getElementById('displayInput');
-const addButton = document.getElementById('addButton');
-const divideButton = document.getElementById('divideButton');
-const multiplyButton = document.getElementById('multiplyButton');
-const subtractButton = document.getElementById('subtractButton');
 const AC = document.getElementById('AC');
 const equal = document.getElementById('equal');
 const operators = document.getElementsByClassName('operator');
 const decimal = document.getElementById('decimal');
 const delButton = document.getElementById('Del');
 const sumBox = document.getElementById('sumBox');
-const negPos = document.getElementById('negPos');
+const divButtons = document.getElementById('buttons');
 
-let sum 
-let input 
-let operatorChosen 
+let sum;
+let input;
+let operatorChosen; 
 let displayVal;
 let equalClicked;
-let decimalClicked;
+let decimalClicked; 
+
+divButtons.addEventListener('click', (e) =>{
+  const isButton = e.target.nodeName === 'INPUT';
+  if (!isButton) {
+    return;
+  }
+  let value = e.target.value;
+  switch (value){
+    case 'AC': 
+      buttonAC();
+      break;
+    case 'Del':
+      del();
+      break;
+    case 'x': 
+      multiplyButton();
+      break;
+    case '+':
+      addButton();
+      break;
+    case '-': 
+      subtractButton();
+      break;
+    case '÷':
+      divideButton();
+      break;
+    case '.': 
+      decimalButton();
+      break;
+    case '=':
+      equalButton();
+      break;
+    case '+/-':
+      negPos();
+      break;
+  } 
+})
 
 //Displayed numbers
 for (i = 0; i <buttons.length; i++){
-  buttons[i].addEventListener('click', displayValue)
+  buttons[i].addEventListener('click', displayValue);
 }
 
 function displayValue(e){
@@ -37,7 +70,18 @@ function displayValue(e){
 }   
 
 function clearDisplay(){
-	input = Number(displayInput.innerHTML);
+  if(displayInput.innerHTML === undefined) {
+    
+  } else if (displayInput.innerHTML == ''){
+    if(operatorChosen !== 'add' && operatorChosen !== 'subtract') {
+      input = 1;
+    } else {
+      input = 0;
+    }
+  } else {
+    input = Number(displayInput.innerHTML);
+  }
+  
 	displayInput.innerHTML = '';
 }
 
@@ -61,10 +105,11 @@ function updateDisplay(){
 
 
 //other buttons
-negPos.addEventListener('click', () => {  //FIX
+function negPos(){
   let result = Number(displayInput.innerHTML);
   result = result * (-1);
   displayInput.innerHTML = result;
+  sumBox.innerHTML = '';
   if (equalClicked === true){
     sum = result;
   }
@@ -82,18 +127,9 @@ negPos.addEventListener('click', () => {  //FIX
     displayCalculation.innerHTML = endString + ' (' + result + ')';
   } else {
     displayCalculation.innerHTML = endString + result;
-  }
-  
-})
-
-function disableDecimal(){
-	if (decimalClicked === true){
-  	decimal.disabled = true;
-    decimalClicked = false;
-  } else {
-  	decimal.disabled = true;
-  }
+  } 
 }
+
 
 function disableOperators(){
 	document.querySelectorAll('input[type="button"].operator').forEach(elem => {
@@ -107,14 +143,24 @@ function disableNumbers(){
 	});
 }
 
-decimal.addEventListener('click', () =>{
+function decimalButton(){
 	decimal.disabled = false;
   displayInput.innerHTML = displayInput.textContent + '.';
   displayCalculation.innerHTML = displayCalculation.textContent + '.';
    sumBox.innerHTML = '';
   decimalClicked = true;
   disableDecimal();
-})
+}
+
+function disableDecimal(){
+	if (decimalClicked === true){
+  	decimal.disabled = true;
+    decimalClicked = false;
+  } else {
+  	decimal.disabled = true;
+  }
+}
+
 
 for (i = 0; i < operators.length; i++){
   operators[i].addEventListener('click', () => {
@@ -123,7 +169,7 @@ for (i = 0; i < operators.length; i++){
   })
 } 
 
-AC.addEventListener('click', () => {
+function buttonAC(){
   clearDisplay();  
   displayCalculation.innerHTML = '';
   sumBox.textContent = '';
@@ -139,9 +185,9 @@ AC.addEventListener('click', () => {
   document.querySelectorAll('input[type="button"].number').forEach(elem => {
     elem.disabled = false;
 	});
-})
+}
 
-equal.addEventListener('click', () => {
+function equalButton(){
   clearDisplay();
   equalClicked = true;
   operate(operatorChosen, sum, input);
@@ -150,9 +196,9 @@ equal.addEventListener('click', () => {
   operatorChosen = undefined;
   delButton.disabled = true;
   decimal.disabled = false;
-})
+}
 
-delButton.addEventListener('click', () => {
+function del(){
   if (equalClicked === true){
     delButton.disabled = true;
     equalClicked = false;
@@ -162,70 +208,85 @@ delButton.addEventListener('click', () => {
     displayInput.innerHTML = Number(result);
 
     let calcString= displayCalculation.textContent.toString();
-    let result2 = calcString.slice(0, calcString.length - 1);
+    let result2
+    if(calcString.slice(-1) == ' ') {
+      result2 = calcString.slice(0, calcString.length - 2);
+    } else {
+      result2 = calcString.slice(0, calcString.length - 1); 
+    }
     displayCalculation.textContent = result2;
   }
+
   
-})
+}
  
 //Operators
 
 function add(a, b){
-    sum = Number((Math.round((a + b) * 100)/100).toFixed(3));
-    return sum;
+  sum = Number((Math.round((a + b) * 100)/100).toFixed(3));
+  return sum;
 }
 function subtract(a, b){
   sum = Number((Math.round((a - b) * 100)/100).toFixed(3));
   return sum;
 }
 function multiply(a, b){
-  sum = Number((Math.round((a * b) * 100)/100).toFixed(3));
-  return sum;
+  if (b !== 0){
+    sum = Number((Math.round((a * b) * 100)/100).toFixed(3));
+    return sum;
+  } else if (b === 0){
+    let newStr = displayCalculation.innerHTML.toString();
+    if (!(newStr.indexOf('x') > -1)){  //reset operator if operatorChosen is deleted
+      operatorChosen = undefined;
+      operatorChosen = undefined;
+     }
+  }
 }
-
 function divide(a, b){
   if (b !== 0){
   	sum = Number((Math.round((a/b) * 100) / 100).toFixed(3));
  	  return sum;
   } else if (b === 0){
-  	 sum = 'ERROR';
-     disableOperators();
-     disableNumbers();
-     delButton.disabled = true;
+  	let newStr = displayCalculation.innerHTML.toString();
+    if (!(newStr.indexOf('÷') > -1)){ //reset operator if operatorChosen is deleted
+      operatorChosen = undefined;
+     } else {
+      sum = 'ERROR';
+      disableOperators();
+      disableNumbers();
+      delButton.disabled = true;
+     }
   }
 }
 
-addButton.addEventListener('click', () => {
+function addButton(){
   if(operatorChosen === undefined){
     operatorChosen = 'add';
   }  
-	
+
   clearDisplay();
-	
+
   if (equalClicked === true){
     input = 0;
     equalClicked = false;
   }
-  
-  if (sum === undefined){
-  	displayCalculation.innerHTML = displayCalculation.textContent + ' + ';
-  } else {
-  	displayCalculation.innerHTML = sum + ' + ';
-  }
 
   if (sum === undefined){
+  	displayCalculation.innerHTML = displayCalculation.textContent + ' + ';
     a = 0;
     b = input;
   } else {
+  	displayCalculation.innerHTML = sum + ' + ';
     a = sum;
     b = input; 
   }
+
   operate(operatorChosen, a, b);
   operatorChosen = 'add';
   sumBox.textContent = sum;
-});
+}
 
-subtractButton.addEventListener('click', () => {
+function subtractButton(){
   if(operatorChosen === undefined) {
     operatorChosen = 'subtract';
   }
@@ -239,51 +300,48 @@ subtractButton.addEventListener('click', () => {
 
   if (sum === undefined){
   	displayCalculation.innerHTML = displayCalculation.textContent + ' - ';
-  } else {
-  	displayCalculation.innerHTML = sum + ' - ';
-  }
-
-  if (sum  === undefined){
     a = input;
     b = 0;
   } else {
+  	displayCalculation.innerHTML = sum + ' - ';
     a = sum;
     b = input;
   }
-  operate(operatorChosen, a, b);
-  sumBox.textContent = sum;
 
-});
-multiplyButton.addEventListener('click', () => {
+  operate(operatorChosen, a, b);
+  operatorChosen = 'subtract';
+  sumBox.textContent = sum;
+}
+
+function multiplyButton(){
+
   if(operatorChosen === undefined) {
     operatorChosen = 'multiply';
   }
+
   clearDisplay();
 
   if (equalClicked === true){
     input = 1;
     equalClicked = false;
   }
-
+  
   if (sum === undefined){
   	displayCalculation.innerHTML = displayCalculation.textContent + ' x ';
-  } else {
-  	displayCalculation.innerHTML = sum + ' x ';
-  }
-
-  if (sum === undefined){
     a = 1;
     b = input;
   } else {
+  	displayCalculation.innerHTML = sum + ' x ';
     a = sum;
     b = input;
   }
+
   operate(operatorChosen, a, b);
   operatorChosen = 'multiply';
   sumBox.textContent = sum;
+}
 
-});
-divideButton.addEventListener('click', () => {
+function divideButton(){
   if(operatorChosen === undefined) {
     operatorChosen = 'divide';
   }
@@ -294,24 +352,20 @@ divideButton.addEventListener('click', () => {
     equalClicked = false;
   }
 
-  if (sum === undefined){
+  if (sum === undefined){ 
   	displayCalculation.innerHTML = displayCalculation.textContent + ' ÷ ';
-  } else {
-  		displayCalculation.innerHTML = sum + ' + ';
-  }
-
-  if (sum === undefined){
     a = input;
     b = 1;
   } else {
+    displayCalculation.innerHTML = sum + ' ÷ ';
     a = sum;
     b = input;
   }
+
   operate(operatorChosen, a, b);
 	operatorChosen = 'divide';
   sumBox.textContent = sum;
-
-});
+}
 
 function operate(operator, a, b){
   if (operator === 'subtract'){
